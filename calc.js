d66e881fac029cafd6eabb28ffa9f7165e949350
rohitrathore930 <rohitrathore8706@gmail.com>
2024-11-29 14:28:15 +0530

    index.html

---
+++ calc.js
@@ -0,0 +1,65 @@
+ var x,y,result
+ 
+ function input1(){
+document.getElementById("box") +="1"
+ }
+ function input2(){
+document.getElementById("box") +="2"
+ }
+ function input3(){
+document.getElementById("box") +="3"
+ }
+ function input4(){
+document.getElementById("box") +="4"
+ }
+ function input5(){
+document.getElementById("box") +="5"
+ }
+ function input6(){
+document.getElementById("box") +="6"
+ }
+ function input7(){
+document.getElementById("box") +="7"
+ }
+ function input8(){
+document.getElementById("box") +="8"
+ }
+ function input9(){
+document.getElementById("box") +="9"
+ }
+ function input0(){
+document.getElementById("box") +="0"
+ }
+ function inputplus(){
+document.getElementById("box") +="+"
+ }
+ function inputminus(){
+document.getElementById("box") +="-"
+ }
+ function inputmultiply(){
+document.getElementById("box") +="*"
+ }
+ function inputdivide(){
+document.getElementById("box") +="/"
+ }
+ function inputsin(){
+document.getElementById("box") = Math.sin( document.getElementById("box").value)
+ }
+ function inputcos(){
+document.getElementById("box") =Math.sin( document.getElementById("box").value)
+ }
+ function inputtan(){
+document.getElementById("box") =Math.sin( document.getElementById("box").value)
+ }
+ function inputcot(){
+document.getElementById("box") =Math.sin( document.getElementById("box").value)
+ }
+ function calculate(){
+document.getElementById("box") = eval( document.getElementById("box").value)
+ }
+
+
+
+ 
+
+
